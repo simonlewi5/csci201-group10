9e573d8d6dbd9339c314118ef398fb14a4150491
Alexander Aseme <aseme@usc.edu>
2024-04-02 02:06:09 -0700

    Updated skeleton code

--- game-client/core/src/com/mygdx/game/Egyptian_Ratscrew.java
+++ game-client/core/src/com/mygdx/game/Egyptian_Ratscrew.java
@@ -1,31 +1,30 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Egyptian_Ratscrew extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
+public class Egyptian_Ratscrew extends Game {
+	public BitmapFont font;
+	public SpriteBatch batch;
+
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+		this.setScreen(new MainMenuScreen(this));
 	}
 
-	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
-	
-	@Override
+
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		font.dispose();
 	}
+
 }
