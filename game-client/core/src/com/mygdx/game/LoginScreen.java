392acc140f9eb7eb4faa957bbd4753d886588b37
Alexander Aseme <aseme@usc.edu>
2024-04-21 13:17:00 -0700

    login screen update

--- game-client/core/src/com/mygdx/game/LoginScreen.java
+++ game-client/core/src/com/mygdx/game/LoginScreen.java
@@ -1,5 +1,6 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -8,8 +9,14 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class LoginScreen implements Screen, MessageListener {
+
+    Viewport viewport;
+
     final EgyptianRatscrew game;
     private BitmapFont fontLarge;
     private Texture backgroundImage;
@@ -17,6 +24,9 @@ public class LoginScreen implements Screen, MessageListener {
     private OrthographicCamera camera;
     private String serverMessage;
     private GameWebSocketClient webSocketClient;
+    private Stage stage;
+
+    private final float ASPECT_RATIO = 16 / 9f;
     
     public void messageReceived(String message) {
     	this.serverMessage = message;
@@ -25,8 +35,18 @@ public class LoginScreen implements Screen, MessageListener {
 
     public LoginScreen(final EgyptianRatscrew game) {
         this.game = game;
+
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 480);
+
+        camera.setToOrtho(false, 800, 800 / ASPECT_RATIO);
+        viewport = new FitViewport(1600, 1600 / ASPECT_RATIO, camera);
+        camera.setToOrtho(false, 800, 800 / ASPECT_RATIO);
+
+        backgroundImage = game.assetManager.getBackgroundImage();
+        backgroundMusic = game.assetManager.getBackgroundMusic();
+
+        // Add a resize listener to handle window resizing
+        Gdx.graphics.setResizable(true);
     }
     
     @Override
@@ -37,21 +57,23 @@ public class LoginScreen implements Screen, MessageListener {
 
         backgroundMusic.setLooping(true);
         backgroundMusic.play();
+
+        Gdx.input.setInputProcessor(stage);
     }
 
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(0, 0, 0.2f, 1);
-        camera.update();
+        ScreenUtils.clear(0, 0, 0, 1);
 
-        game.batch.setProjectionMatrix(camera.combined);
+        viewport.apply();
+        game.batch.setProjectionMatrix(viewport.getCamera().combined);
         game.batch.begin();
-        game.batch.draw(backgroundImage, 0, 0, 800, 480);
+        game.batch.draw(backgroundImage, 0, 0, viewport.getWorldWidth(), viewport.getWorldHeight());
 
         GlyphLayout layout = new GlyphLayout(fontLarge, "LOGIN SCREEN");
-        float x = (800 - layout.width) / 2;
-        float y = (480 + layout.height) / 2;
+        float x = (viewport.getWorldWidth() - layout.width) / 2;
+        float y = (viewport.getWorldHeight() - layout.height) / 2;
         fontLarge.setColor(Color.WHITE);
         fontLarge.draw(game.batch, layout, x, y);
 
@@ -60,6 +82,8 @@ public class LoginScreen implements Screen, MessageListener {
 
     @Override
     public void resize(int width, int height) {
+        viewport.update(width, height, true);
+//        stage.getViewport().update(width, height, true);
     }
 
 
