6325dffb63b147c6e93249d33348fd9de04aec47
Simon Lewis <lewissimonjohn@gmail.com>
2024-04-26 15:22:28 -0700

    some missing intialization

--- game-server/pkg/models/match.go
+++ game-server/pkg/models/match.go
@@ -17,16 +17,16 @@ const (
 )
 
 type Match struct {
-	ID           string          `json:"id"`
-	Players      []*Player       `json:"players"`
-	Hands        map[string]Hand `json:"hands"`         //map of player username to hand
-	MatchState   MatchState      `json:"match_state"`
-	TurnIndex    int             `json:"turn_index"`
-	Winner       Player          `json:"winner"`
-	StartTime    int64           `json:"start_time"`
-	EndTime      int64           `json:"end_time"`
-	Deck         Deck            `json:"deck"`
-	CenterPile   CenterPile      `json:"center_pile"`
+	ID         string          `json:"id"`
+	Players    []*Player       `json:"players"`
+	Hands      map[string]Hand `json:"hands"` //map of player username to hand
+	MatchState MatchState      `json:"match_state"`
+	TurnIndex  int             `json:"turn_index"`
+	Winner     Player          `json:"winner"`
+	StartTime  int64           `json:"start_time"`
+	EndTime    int64           `json:"end_time"`
+	Deck       Deck            `json:"deck"`
+	CenterPile CenterPile      `json:"center_pile"`
 }
 
 type MatchRequest struct {
@@ -65,9 +65,12 @@ func NewMatch(players []*Player) *Match {
 		fmt.Println(err)
 	}
 
+	playerHands := make(map[string]Hand)
+
 	match := &Match{
 		ID:         matchID,
 		Players:    players,
+		Hands:      playerHands,
 		MatchState: MatchStateInit,
 		TurnIndex:  firstPlayerIdx,
 		//Winner is set to an empty player to indicate that the match is still in progress
@@ -76,18 +79,21 @@ func NewMatch(players []*Player) *Match {
 		//EndTime is set to 0 to indicate that the match is still in progress
 		EndTime: 0,
 		Deck:    deck,
+		CenterPile: CenterPile{
+			Cards: []Card{},
+		},
 	}
 
 	for len(match.Deck.Cards) > 0 {
 		for i := 0; i < len(players); i++ {
 			currentPlayerIndex := (firstPlayerIdx + 1 + i) % len(players)
 			player := players[currentPlayerIndex]
-			
+
 			if len(match.Deck.Cards) == 0 {
 				break
 			}
 			card := match.Deck.DrawCard()
-	
+
 			hand := match.Hands[player.Username]
 			hand.Cards = append(hand.Cards, card)
 			match.Hands[player.Username] = hand
@@ -102,13 +108,13 @@ func generateMatchID() string {
 }
 
 func getRandomIndex(n int) (int, error) {
-    if n == 0 {
-        return 0, fmt.Errorf("getRandomIndex: the slice is empty")
-    }
-    bigN := big.NewInt(int64(n))
-    bigIdx, err := rand.Int(rand.Reader, bigN)
-    if err != nil {
-        return 0, err
-    }
-    return int(bigIdx.Int64()), nil
+	if n == 0 {
+		return 0, fmt.Errorf("getRandomIndex: the slice is empty")
+	}
+	bigN := big.NewInt(int64(n))
+	bigIdx, err := rand.Int(rand.Reader, bigN)
+	if err != nil {
+		return 0, err
+	}
+	return int(bigIdx.Int64()), nil
 }
